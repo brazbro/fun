4d07279c7e64b32c53c7ab002277032eb91e21eb
Brian J. Besterman <bbesterman@wdtablesystems.com>
2018-05-14 16:56:46 -0400

    Added optimal algorithm for Best Stock Profit (Java)

--- jvm-fun/src/main/java/net/besterman/problems/BestStockProfitJava.java
+++ jvm-fun/src/main/java/net/besterman/problems/BestStockProfitJava.java
@@ -1,7 +1,5 @@
 package net.besterman.problems;
 
-import java.util.LinkedList;
-
 /**
  * Problem: given a list of minute-by-minute stock prices where the indices are the time (in minutes) past trade opening time, and
  * the values are the price (in US dollars) of one share of Apple stock at that time, write an efficient function that
@@ -11,64 +9,52 @@ import java.util.LinkedList;
 public class BestStockProfitJava {
 
     /**
-     * The approach is to realize that as long as the stock price is dropping, you can ignore everything but the lowest price,
-     * and as long as the price is increasing, you can ignore everything but the highest price. These are local minimums and
-     * maximums which you can extract into a new list in a single pass.
-     * Then you make a second pass to calculate the largest interval between consecutive points.
-     * @param stockPrices a minute-by-minute array of stock prices, where the indices are the time (in minutes) pas trade opening time
+     * @param stockPrices a minute-by-minute array of stock prices, where the indices are the time (in minutes) past trade opening time
      * @return the best profit one could have made from one purchase and one sale of one share
      */
     public static int calculateMaxProfit(int[] stockPrices) {
         assert stockPrices != null && stockPrices.length > 1 : "At least 2 stock prices are required";
-        LinkedList<Integer> prunedList = new LinkedList<>();
-        boolean rising = true;
+        Integer min = null;
+        Integer max = null;
+        Integer bestProfit = null;
         for (int price : stockPrices) {
-            if (prunedList.isEmpty()) {
-                prunedList.add(price);
-            } else if (price != prunedList.getLast()) { // ignore any prices that are same as previous price
-                if (prunedList.size() == 1) {
-                    // Determine initial direction
-                    rising = (price > prunedList.getLast());
-                    prunedList.add(price); // now that we have 2 elements, we can start pruning if direction does not change
-                } else if (rising == (price > prunedList.getLast())) {
-                    // going in same direction, so eliminate previous price
-                    prunedList.removeLast();
-                    prunedList.add(price);
-                } else {
-                    // Change of direction
-                    prunedList.add(price);
-                    rising = !rising;
+            if (min == null) {
+                min = price;
+            } else if (price < min) {
+                if (max == null) {
+                    if (bestProfit == null) {
+                        bestProfit = price - min;
+                    } else {
+                        bestProfit = Math.max(bestProfit, price - min);
+                    }
+                }
+                min = price;
+                max = null;
+            } else if (max == null || price >= max) {
+                max = price;
+                if (bestProfit == null || max - min > bestProfit) {
+                    bestProfit = max - min;
                 }
             }
         }
+        assert bestProfit != null;
+        return  bestProfit;
+    }
 
-        // Special case of a constant price: no trend == no profit
-        if (prunedList.size() == 1) {
-            return 0;
-        }
-
-        // Special case of single downward trend: find minimum loss between prices
-        if (prunedList.size() == 2 && prunedList.getFirst() > prunedList.getLast()) {
-            int low = prunedList.getLast();
-            int minLoss = Integer.MIN_VALUE;
-            Integer lastPrice = null;
-            for (int price : stockPrices) {
-                if (lastPrice != null) {
-                    minLoss = Math.max(minLoss, price - lastPrice);
-                }
-                if (price == low) {
-                    break;
-                }
-                lastPrice = price;
-            }
-            return minLoss;
-        }
-
-        int maxProfit = Integer.MIN_VALUE;
-        int lastPrice = prunedList.removeFirst();
-        for (int price : prunedList) {
-            maxProfit = Math.max(maxProfit, price - lastPrice);
-            lastPrice = price;
+    /**
+     * Optimal algorithm relies on defining the potential profit at any time as (current price - minimum price seen so far).
+     * This must account for the potential of a loss as the maximum "profit".
+     */
+    public static int optimalAlgorithm(int[] stockPrices) {
+        assert stockPrices != null && stockPrices.length > 1 : "At least 2 stock prices are required";
+        int numPrices = stockPrices.length;
+        int minPrice = stockPrices[0];
+        int maxProfit = stockPrices[1] - minPrice;
+        for (int t = 1; t < numPrices; t++) {
+            int currentPrice = stockPrices[t];
+            int potentialProfit = currentPrice - minPrice;
+            maxProfit = Math.max(maxProfit, potentialProfit);
+            minPrice = Math.min(minPrice, currentPrice);
         }
         return maxProfit;
     }
